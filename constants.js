3d07a7900f2eda1fe196c5f1157c21362ed79c7b
Xirox <Kuikka87@gmail.com>
2019-04-08 01:16:08 +0300

    Added 'desciption' field to BotCommands

--- constants.js
+++ constants.js
@@ -1,21 +1,31 @@
 const BotCommands = {
     "help":{
         "command":"!help",
+        "description":
+            "Show useful information about a command " +
+            "or show list of commands.",
         "syntax":"!help ?<subject>",
         "example":"!help win"
     },
     "getupdate":{
         "command":"!getupdate",
+        "description":
+            "Sends the latest CS:GO update article to the " +
+            "channel where this command gets called.",
         "syntax":"!getupdate",
         "example":"!getupdate"
     },
     "schedulestart":{
         "command":"!schedulestart",
+        "description":
+            "Starts CS:GO update schedule. Only available to admin.",
         "syntax":"!schedulestart",
         "example":"!schedulestart"
     },
     "schedulestop":{
         "command":"!schedulestop",
+        "description":
+            "Stops CS:GO update schedule. Only available to admin.",
         "syntax":"!schedulestop",
         "example":"!schedulestop"
     }
