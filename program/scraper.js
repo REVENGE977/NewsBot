cc98893384e5f93a7ac8bfebfdb59416c231ec75
Zecuel <Kuikka87@gmail.com>
2019-04-12 13:32:50 +0300

    Changed scraper structure

--- program/scraper.js
+++ program/scraper.js
@@ -1,56 +1,58 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
-const url = "https://blog.counter-strike.net/index.php/category/updates/";
+const CSGOUrl = "https://blog.counter-strike.net/index.php/category/updates/";
 
 async function GetNewestCSGOUpdate(){
-    let link = await GetLatestNewsLink();
-    let title = await GetNewsTitle(link);
-    let body = await GetNewsBody(link);
+    let link = await CSGO.GetNewsLink();
+    let title = await CSGO.GetNewsTitle(link);
+    let body = await CSGO.GetNewsBody(link);
 
     return [link,title,body];
 }
 
-function GetLatestNewsLink(){
-    return new Promise((resolve, reject) => {
-        rp(url).then((html) => {
-            resolve($("h2 > a", html)[0].attribs.href);
-        }).catch((error) => { return reject(error); });
-    });
-}
+class CSGO {
+    static GetNewsLink(){
+        return new Promise((resolve, reject) => {
+            rp(CSGOUrl).then((html) => {
+                resolve($("h2 > a", html)[0].attribs.href);
+            }).catch((error) => { return reject(error); });
+        });
+    }
 
-function GetNewsTitle(link){
-    return new Promise((resolve, reject) => {
-        rp(link).then((html) => {
-            resolve($("h2 > a", html)[0].children[0].data);
-        }).catch((error) => { return reject(error); })
-    });
-}
-function GetNewsBody(link){
-    return new Promise((resolve, reject) => {
-        let output = [];
-        rp(link).then((html) => {
+    static GetNewsTitle(link){
+        return new Promise((resolve, reject) => {
+            rp(link).then((html) => {
+                resolve($("h2 > a", html)[0].children[0].data);
+            }).catch((error) => { return reject(error); })
+        });
+    }
+    static GetNewsBody(link){
+        return new Promise((resolve, reject) => {
+            let output = [];
+            rp(link).then((html) => {
 
-            let childCount = $("div.inner_post p", html).length;
-            for (let i = 1; i < childCount; i++){
-                let childCount = $("div.inner_post p", html)[i].children.length;
-                for (let y = 0; y < childCount; y++){
-                    let _data = ($("div.inner_post p", html)[i].children[y].data);
-                    if (_data && _data !== ""){
-                        output.push(_data.replace(/\n/g, "") + "\n");
+                let childCount = $("div.inner_post p", html).length;
+                for (let i = 1; i < childCount; i++){
+                    let childCount = $("div.inner_post p", html)[i].children.length;
+                    for (let y = 0; y < childCount; y++){
+                        let _data = ($("div.inner_post p", html)[i].children[y].data);
+                        if (_data && _data !== ""){
+                            output.push(_data.replace(/\n/g, "") + "\n");
+                        }
                     }
                 }
-            }
 
-            let body = undefined;
-            output.forEach((line) => {
-                body += line;
-                if (!line.endsWith("\n")){ body += "\n"; }
-            });
-            body += "\n";
+                let body = undefined;
+                output.forEach((line) => {
+                    body += line;
+                    if (!line.endsWith("\n")){ body += "\n"; }
+                });
+                body += "\n";
 
-            resolve(body);
-        }).catch((error) => { return reject(error); })
-    });
+                resolve(body);
+            }).catch((error) => { return reject(error); })
+        });
+    }
 }
 
 module.exports.GetNewestCSGOUpdate = GetNewestCSGOUpdate;
