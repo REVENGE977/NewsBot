dc69443cbf523702a4c624edd1cf78848fb59996
Zecuel <Kuikka87@gmail.com>
2019-04-15 00:43:36 +0300

    Moved SendNewsArticle to functions.js

--- program/misc/functions.js
+++ program/misc/functions.js
@@ -1,3 +1,6 @@
+const ScraperHandler = require('../scraperhandler');
+const Discord = require("discord.js");
+
 function GetCommandDescription(command){
     let output = "\n";
     output += "Name:                 " + command.name + "\n";
@@ -14,6 +17,93 @@ function GetCommandDescription(command){
     return output;
 }
 
+async function SendNewsArticle(game, channels = [], sender = "user", ){
+
+    console.log("Sending news article...");
+
+    let scraperOutput, link, title, body, image, messageTitle;
+
+    if (sender === "bot"){
+        channels = await this.Database.GetChannels(game);
+    }
+
+    switch(game){
+        case 'csgo':
+            image = "counter_strike_wallpaper.png";
+
+            scraperOutput = await ScraperHandler.GetCSGOUpdate();
+
+            link = scraperOutput[0]; title = scraperOutput[1]; body = scraperOutput[2];
+
+            messageTitle = "__**Latest CS:GO news:**__\n";
+
+            if (sender === "bot"){
+                messageTitle = "__**New CS:GO update released!**__\n";
+                if (await this.Database.NewsArticleExists(game,title)){ return console.log("Old article."); }
+            }
+
+
+            body = body.replace(/(\[[A-Za-z0-9]+])/g, (original) => {
+                return "\n**" + original + "**";
+            });
+            body = body.replace(/([A-Za-z0-9]+[:])/g, (original) => {
+                return "\n**" + original + "**";
+            });
+
+            break;
+        case 'osrs':
+            image = "oldschool.png";
+
+            scraperOutput = await ScraperHandler.GetOSRSUpdate();
+
+            link = scraperOutput[0]; title = scraperOutput[1]; body = scraperOutput[2];
+
+            messageTitle = "__**Latest OSRS news:**__\n\n";
+
+            if (sender === "bot"){
+                messageTitle = "__**New OSRS update release!**__\n\n";
+                if (await this.Database.NewsArticleExists(game,title)){ return console.log("Old article"); }
+            }
+
+            messageTitle += "__**Update topics**__: \n";
+
+            break;
+        default:
+            throw new Error("Invalid game: " + game);
+    }
+
+    if (!scraperOutput || !channels){
+        throw new Error("Invalid arguments in command.");
+    }
+
+    body = body.replace(undefined, "");
+
+    const embed = new Discord.RichEmbed()
+        .setTitle("__**" + title + "**__")
+        .setURL(link)
+        .setColor(524419);
+
+    if (image) {
+        embed.attachFiles(["./images/" + image])
+            .setImage("attachment://" + image);
+    }
+
+    if (sender === "bot"){
+        if (!await this.Database.AddNewsArticle(game,title)){
+            return console.log("Error while adding news article to DB!");
+        }
+    }
+
+    return {
+        name: "sendupdate",
+        channels: channels,
+        messageTitle: messageTitle,
+        body: body,
+        embed: embed
+    }
+}
+
 module.exports = {
-    GetCommandDescription: GetCommandDescription
+    GetCommandDescription: GetCommandDescription,
+    SendNewsArticle: SendNewsArticle
 };
--- program/scheduler.js
+++ program/scheduler.js
@@ -1,7 +1,6 @@
 const cron = require('node-cron');
-const Discord = require('discord.js');
-const ScraperHandler = require("./scraperhandler");
 const DatabaseCL = require("./database").Database;
+const functions = require('./misc/functions');
 
 class Scheduler {
     constructor(Timer = "0 */30 * * * *"){
@@ -10,8 +9,8 @@ class Scheduler {
         this.Database = new DatabaseCL();
         this.CRONSchedule = cron.schedule(Timer, () => {
             console.log("News getter scheduled...");
-            this.SendNewsArticle("csgo",[],"bot");
-            this.SendNewsArticle("csgo",[],"bot");
+            functions.SendNewsArticle("csgo",[],"bot");
+            functions.SendNewsArticle("csgo",[],"bot");
             }, { scheduled: false, timezone: "Europe/Helsinki" });
     }
 
@@ -26,92 +25,6 @@ class Scheduler {
         console.log("Stopping cron schedule...");
         this.CRONSchedule.stop();
         return "Cron schedule stopped successfully.";
-    }
-
-    async SendNewsArticle(game, channels = [], sender = "user", ){
-
-        console.log("Sending news article...");
-
-        let scraperOutput, link, title, body, image, messageTitle;
-
-        if (sender === "bot"){
-            channels = await this.Database.GetChannels(game);
-        }
-
-        switch(game){
-            case 'csgo':
-                image = "counter_strike_wallpaper.png";
-
-                scraperOutput = await ScraperHandler.GetCSGOUpdate();
-
-                link = scraperOutput[0]; title = scraperOutput[1]; body = scraperOutput[2];
-
-                messageTitle = "__**Latest CS:GO news:**__\n";
-
-                if (sender === "bot"){
-                    messageTitle = "__**New CS:GO update released!**__\n";
-                    if (await this.Database.NewsArticleExists(game,title)){ return console.log("Old article."); }
-                }
-
-
-                body = body.replace(/(\[[A-Za-z0-9]+])/g, (original) => {
-                    return "\n**" + original + "**";
-                });
-                body = body.replace(/([A-Za-z0-9]+[:])/g, (original) => {
-                    return "\n**" + original + "**";
-                });
-
-                break;
-            case 'osrs':
-                image = "oldschool.png";
-
-                scraperOutput = await ScraperHandler.GetOSRSUpdate();
-
-                link = scraperOutput[0]; title = scraperOutput[1]; body = scraperOutput[2];
-
-                messageTitle = "__**Latest OSRS news:**__\n\n";
-
-                if (sender === "bot"){
-                    messageTitle = "__**New OSRS update release!**__\n\n";
-                    if (await this.Database.NewsArticleExists(game,title)){ return console.log("Old article"); }
-                }
-
-                messageTitle += "__**Update topics**__: \n";
-
-                break;
-            default:
-                throw new Error("Invalid game: " + game);
-        }
-
-        if (!scraperOutput || !channels){
-            throw new Error("Invalid arguments in command.");
-        }
-
-        body = body.replace(undefined, "");
-
-        const embed = new Discord.RichEmbed()
-            .setTitle("__**" + title + "**__")
-            .setURL(link)
-            .setColor(524419);
-
-        if (image) {
-            embed.attachFiles(["./images/" + image])
-                .setImage("attachment://" + image);
-        }
-
-        if (sender === "bot"){
-            if (!await this.Database.AddNewsArticle(game,title)){
-                return console.log("Error while adding news article to DB!");
-            }
-        }
-
-        return {
-            name: "sendupdate",
-            channels: channels,
-            messageTitle: messageTitle,
-            body: body,
-            embed: embed
-        }
     }
 }
 
